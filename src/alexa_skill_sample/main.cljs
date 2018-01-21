fcb0763ab9a3a7fdfc0c2ab029adc5da962076b2
OHTA Shogo <athos0220@gmail.com>
2018-01-21 16:44:32 +0900

    Improve conversation phrases

--- src/alexa_skill_sample/main.cljs
+++ src/alexa_skill_sample/main.cljs
@@ -7,15 +7,19 @@
 
 (def handlers
   {:LaunchRequest (fn [this]
-                    (.emit this ":tell" "こんにちは"))
+                    (.emit this ":ask" "コワーキングスペース、ハレイクについてご案内します。メニューを知りたい場合はヘルプ、とお伝え下さい。終了する場合は終了、とお伝え下さい。" ""))
+   :BusinessDayIntent (fn [this]
+                        (.emit this ":ask" "ハレイクは水曜以外のすべての曜日で営業しています。"))
+   :FinishIntent (fn [this]
+                   (.emit this "AMAZON.StopIntent"))
    :Unhandled (fn [this]
-                (.emit this ":tell" "すみません、よく分かりません。"))
+                (.emit this ":ask" "すみません、よく分かりませんでした。" "もう一度メニューをお選び下さい。"))
    :AMAZON.HelpIntent (fn [this]
-                        (.emit this ":tell" "ヘルプはありません"))
+                        (.emit this ":ask" "ご利用可能なメニューは次のとおりです。「営業日」でハレイクの営業日の確認ができます。" ""))
    :AMAZON.CancelIntent (fn [this]
                           (.emit this "AMAZON.StopIntent"))
    :AMAZON.StopIntent (fn [this]
-                        (.emit this ":tell" "さようなら"))})
+                        (.emit this ":tell" "ご利用ありがとうございました"))})
 
 (defn map->handlers [m]
   (clj->js (into {} (map (fn [[k f]] [k #(this-as this (f this))])) m)))
