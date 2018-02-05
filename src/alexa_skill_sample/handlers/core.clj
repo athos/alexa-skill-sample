954ba9dcf24bd91437d00221418c3deb00dc8599
OHTA Shogo <athos0220@gmail.com>
2018-02-05 13:41:08 +0900

    Make `this` statically scoped to prevent it from being bound to wrong object

--- src/alexa_skill_sample/handlers/core.clj
+++ src/alexa_skill_sample/handlers/core.clj
@@ -1,5 +1,11 @@
 (ns alexa-skill-sample.handlers.core)
 
 (defmacro defhandler [intent & body]
-  `(defmethod handle-intent ~intent [_#]
-     ~@body))
+  `(do (swap! %handlers assoc ~intent #(cljs.core/this-as ~'&self ~@body))
+       ~intent))
+
+(defmacro emit [& args]
+  `(.emit ~'&self ~@args))
+
+(defmacro t [x]
+  `(.t ~'&self ~x))
--- src/alexa_skill_sample/handlers/core.cljs
+++ src/alexa_skill_sample/handlers/core.cljs
@@ -1,21 +1,7 @@
 (ns alexa-skill-sample.handlers.core
   (:require-macros [alexa-skill-sample.handlers.core]))
 
-(defmulti handle-intent identity)
-
-(def ^:private ^:dynamic *self*)
-
-(defn emit
-  ([x] (.emit *self* x))
-  ([x y] (.emit *self* x y))
-  ([x y z] (.emit *self* x y z)))
-
-(defn t [x]
-  (.t *self* x))
+(def %handlers (atom {}))
 
 (defn handlers []
-  (->> (methods handle-intent)
-       (into {} (map (fn [[k f]]
-                       [k #(this-as this
-                             (binding [*self* this] (f k)))])))
-       clj->js))
+  (clj->js @%handlers))
